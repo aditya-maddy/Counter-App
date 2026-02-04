8124fbe4c475396dcc429abc653d60245e899104
aditya mahadik <adityamahadik6530@gmail.com>
2026-02-04 23:58:26 +0530

    Counter App

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+import Counter from "./counter";
+
+function App() {
+  return (<>
+  <h1> counter app</h1>
+  <Counter/>
+  </>)  
+}
+
+export default App;
